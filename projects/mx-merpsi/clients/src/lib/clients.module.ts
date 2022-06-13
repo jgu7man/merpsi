26b0efdc30131869a39d6801dbbb7cb9c7555150
Mariana Barrera <mebz1991@gmail.com>
2022-06-13 11:12:42 -0400

    creacion de client modulo como libreria

---
+++ projects/mx-merpsi/clients/src/lib/clients.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ClientsComponent } from './clients.component';
+
+
+
+@NgModule({
+  declarations: [
+    ClientsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ClientsComponent
+  ]
+})
+export class ClientsModule { }
---
+++ projects/mx-merpsi/clients/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of clients
+ */
+
+export * from './lib/clients.service';
+export * from './lib/clients.component';
+export * from './lib/clients.module';
