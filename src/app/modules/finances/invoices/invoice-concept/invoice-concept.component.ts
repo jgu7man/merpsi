39a77b48ff012298748f6ded977ddceb8886df3e
jorge gu7man <jgu7man@gmail.com>
2022-07-06 09:38:16 -0500

    cambios en las validaciones del html de facturas

--- src/app/modules/finances/invoices/invoice-concept/invoice-concept.component.ts
+++ src/app/modules/finances/invoices/invoice-concept/invoice-concept.component.ts
@@ -13,29 +13,32 @@ import { iSalesInvoice } from '../../sales-invoices/sales-invoice.model';
 import { SalesService } from '../../sales-invoices/sales.service';
 import { InvoiceConceptService } from './invoice-concept.service';
 
-@Component({
+@Component( {
   selector: 'app-invoice-concept',
   templateUrl: './invoice-concept.component.html',
-  styleUrls: ['./invoice-concept.component.scss']
-})
+  styleUrls: [ './invoice-concept.component.scss' ]
+} )
 export class InvoiceConceptComponent implements OnInit, OnDestroy {
 
   @Input() invoice: iSalesInvoice | null = null
-  @Input() document: string = ''
+
+  // NOTE Los tipados de propiedades que funcionan como flag, es preferible limitar su valor con el tipado de sus posibles valores nada más
+  @Input() document?: 'sale' | 'credit' | 'debit'
+  // @Input() document: string = ''
   @Input() tipo_concepto?: NoteCredit.context
   @Input() concept: ProductInvoiceModel | Invoice.concept | null = null
-  businessRef = this._cache.getDataKey('eid')
+  businessRef = this._cache.getDataKey( 'eid' )
   productSelect: ProductModel | string = ''
   productListEmpty = false
   stores$: Observable<iSede[]>
 
 
-  formAddProduct: FormGroup = new FormGroup({
-    cant: new FormControl(1,),
-    unit_price: new FormControl(0,),
-  })
+  formAddProduct: FormGroup = new FormGroup( {
+    cant: new FormControl( 1, ),
+    unit_price: new FormControl( 0, ),
+  } )
 
-  constructor(
+  constructor (
     public sales: SalesService,
     public purchase: PurchaseInvoiceService,
     public conceptInvoice: InvoiceConceptService,
@@ -50,59 +53,93 @@ export class InvoiceConceptComponent implements OnInit, OnDestroy {
 
 
   ngOnInit(): void {
-    if (this.concept) {
+    if ( this.concept ) {
       /* validaciones para los conceptos segun el documento en el que este */
-      if (this.document == 'sale') {
-        
-        if (this.conceptInvoice.details$.value) {
-          let details = this.conceptInvoice.details$.value
-          details.map(det => {
-            if (det.product.UPC === this.concept!.product.UPC) {
-              this.formAddProduct.controls.cant.setValidators(Validators.max(det.stock))
-              console.log(det);
-            }
-          })
-        }
-      } else 
-      if (this.document == 'debit') {
-        if (this.conceptInvoice.details_Notes$.value) {
-          let details = this.conceptInvoice.details_Notes$.value
-          details.map(det => {
-            if (det.product.UPC === this.concept!.product.UPC) {
-              this.formAddProduct.controls.unit_price.setValidators(Validators.min(this.concept!.unit_price))
-            }
-          })
-        }
-      }
-      else 
-      if (this.document == 'credit') {
-        if (this.conceptInvoice.details_Notes$.value) {
-          let details = this.conceptInvoice.details_Notes$.value
-          details.map(det => {
-            if (det.product.UPC === this.concept!.product.UPC) {
-              this.formAddProduct.controls.unit_price.setValidators(Validators.max(this.concept!.unit_price))
-              this.formAddProduct.controls.cant.setValidators(Validators.max(this.concept!.cant!))
-            }
-          })
-        }
-      }
-      this.formAddProduct.valueChanges.pipe(
-        distinctUntilChanged((x, y) =>
-          typeof x != 'object' ? x === y : JSON.stringify(x) === JSON.stringify(y)
-        ),
-        skip(1),
-        debounceTime(1000),
-      ).subscribe(changes => {
-        /* Escucha los cambios del formulario de conceptos para realizar los calculos del totales e informar al footer  */
-        if (this.formAddProduct.valid) {
-          this.conceptInvoice.update(changes, this.concept!, this.document);
-        } 
-      })
+      if ( this.document == 'sale' ) {
 
-      this.formAddProduct.patchValue(this.concept)
+        if ( this.conceptInvoice.details$.value ) {
+          let details = this.conceptInvoice.details$.value
+          details.map( det => {
+            if ( det.product.UPC === this.concept!.product.UPC ) {
+              this.formAddProduct.controls.cant.setValidators( Validators.max( det.stock ) )
+              console.log( det );
+            }
+          } )
+        }
+      } else
+        if ( this.document == 'debit' ) {
+          if ( this.conceptInvoice.details_Notes$.value ) {
+            let details = this.conceptInvoice.details_Notes$.value
+            details.map( det => {
+              if ( det.product.UPC === this.concept!.product.UPC ) {
+                this.formAddProduct.controls.unit_price.setValidators( Validators.min( this.concept!.unit_price ) )
+              }
+            } )
+          }
+        }
+        else
+          if ( this.document == 'credit' ) {
+            if ( this.conceptInvoice.details_Notes$.value ) {
+              let details = this.conceptInvoice.details_Notes$.value
+              details.map( det => {
+                if ( det.product.UPC === this.concept!.product.UPC ) {
+                  this.formAddProduct.controls.unit_price.setValidators( Validators.max( this.concept!.unit_price ) )
+                  this.formAddProduct.controls.cant.setValidators( Validators.max( this.concept!.cant! ) )
+                }
+              } )
+            }
+          }
+      this.formAddProduct.valueChanges.pipe(
+        distinctUntilChanged( ( x, y ) =>
+          typeof x != 'object' ? x === y : JSON.stringify( x ) === JSON.stringify( y )
+        ),
+        skip( 1 ),
+        debounceTime( 1000 ),
+      ).subscribe( changes => {
+        /* Escucha los cambios del formulario de conceptos para realizar los calculos del totales e informar al footer  */
+        if ( this.formAddProduct.valid ) {
+          this.conceptInvoice.update( changes, this.concept!, this.document );
+        }
+      } )
+
+      this.formAddProduct.patchValue( this.concept )
       this.formAddProduct.markAsPristine()
     }
 
+  }
+
+  get allowEditCant() {
+    if ( !this.invoice ) return false
+    else if ( this.document == 'debit' ) return false
+    else {
+      if ( this.tipo_concepto == 'anulacion'
+        || this.tipo_concepto == 'disminucion' )
+        return false
+    }
+    return true
+  }
+
+  get allowEditPrice() {
+    if ( !this.invoice ) return false
+    else if ( this.document == 'credit' ) return false
+    else {
+      if ( this.tipo_concepto != 'disminucion' ) return false
+    }
+    return true
+  }
+
+  get itemAmount() {
+    let details = !this.isRelatedDocument
+      ? this.conceptInvoice.details$.value
+      : this.conceptInvoice.details_Notes$.value;
+    let concept = details.find( det => det.product.UPC === this.concept?.product.UPC );
+
+    let amoumt = concept?.amount || 0
+    return amoumt
+  }
+
+  get isRelatedDocument() {
+    return this.document == 'credit' || this.document == 'debit'
   }
 
   getValue(product: ProductModel) {
